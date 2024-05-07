8db369947a8937f4943ed2720436ebd2bc693293
drincon1 <d.rincon@uniandes.edu.co>
2024-05-06 21:28:41 -0500

    registrarse plus log in done

--- src/connection.py
+++ src/connection.py
@@ -1,21 +1,70 @@
-
-from pymongo.mongo_client import MongoClient
+from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from bson import ObjectId
 import os
 
+class MongoDB:
+    
+    DATABASE = 'web_app'
+    
+    def __init__(self):
+        user = os.environ.get("DB_USER")
+        password = os.environ.get("DB_PASS")
+        
+        uri = f"mongodb+srv://{user}:{password}@clusterpdg.psjdd0v.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPDG"
+        
+        self.client = MongoClient(uri, server_api=ServerApi('1'), tls=True)
+        self.database = self.client[self.DATABASE]
+        
+    
+    def iniciar_sesion(self,usuario:str, contrasena:str) -> bool:
+        try:
+            usr = self.get_usuario(usuario=usuario)
+            if usr is None: 
+                return False
+            
+            if usr['contrasena'] != contrasena:
+                return False
+            
+            return True
+            
+        except Exception as e:
+            print(e)
+    
+    def get_usuario(self, usuario: str) -> dict:
+        try:
+            collection = self.database['usuarios']
+            usu_dict = collection.find_one({"usuario": usuario})
+            
+            return usu_dict  # Return the user document if found, None otherwise
+            
+        except Exception as e:
+            print(e)
 
-user = os.environ.get("DB_USER")
-password = os.environ.get("DB_PASS")
+    
+    
+            
+    def create_usuario(self, usuario: str, contrasena: str) -> str:
+        try:
+            if usuario == '' or usuario is None or contrasena is None or contrasena == '':
+                return 'Usuario o Contraseña está vació'
+            
+            existe = self.get_usuario(usuario)
+            if not existe:
+                collection = self.database['usuarios']
+                # Creating a dictionary with student details
+                data = {
+                    '_id': ObjectId(),
+                    'usuario': usuario,
+                    'contrasena': contrasena
+                }
 
-uri = f"mongodb+srv://{user}:{password}@clusterpdg.psjdd0v.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPDG"
-
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'), tls=True)
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+                # Inserting the student data into the 'students' collection and obtaining the inserted ID
+                collection.insert_one(data)
+                
+                return f'{usuario} creado con éxito'
+            
+            return f"El usuario '{usuario}' ya existe"
+        
+        except Exception as e:
+            print(e)
--- src/pages/home_page.py
+++ src/pages/home_page.py
@@ -1,17 +1,30 @@
+import sys
+sys.path.append("..")
 import dash_bootstrap_components as dbc
 import dash 
 from dash import html, Output, Input, State, callback, dcc
+from connection import MongoDB
 
 dash.register_page(__name__,path='/')
+
+mongo = MongoDB()
 
 layout = html.Div(children=[
     html.Div(className="background", children=[
         html.Div(className="home-page", children=[
             html.Div(className="title-buttons", children=[
-                html.H2(className="home-title", children="Autodiagnóstico: Dependencias e impactos sobre el agua como servicio ecosistémico"),
-                html.Div(className="home-buttons", children=[
-                    html.Button(id="btn-comenzar", children="Comenzar"),
-                    dcc.Location(id='url', refresh=True),
+                html.H2(className="home-title", children="Bienvenido"),
+                html.Div(className="home-login", children=[
+                    html.Label("Usuario"),
+                    dcc.Input(id='input-usuario'),
+                    html.Label("Contraseña"),
+                    dcc.Input(type='password',id='input-contrasena'),
+                    html.Div(className='home-buttons',children=[
+                        html.Button(id="btn-registrarse", children="Registrarse"),
+                        html.Button(id="btn-log-in", children="Log-In"),
+                        dcc.Location(id='url', refresh=True),
+                    ]),
+                    html.P(id="error-message",className="error-message")
                 ])
             ]),
             html.Img(className="home-image", src="assets/imagenes/home-image.jpg")
@@ -19,14 +32,39 @@ layout = html.Div(children=[
     ])
 ])
 
-# Callback para ir a la dirección URL /arbol-decision
 @callback(
-    Output('url', 'pathname'),
-    [Input('btn-comenzar', 'n_clicks')]
+    Output('error-message','children',allow_duplicate=True),
+    [State('input-usuario','value'),
+     State('input-contrasena','value')],
+    Input('btn-registrarse','n_clicks'),
+    prevent_initial_call=True
 )
-def change_layout(n_clicks):
-    if n_clicks is not None and n_clicks > 0:
-        return '/cuestionario'
-    else:
+def registrarse(usuario,contrasena,n_clicks):
+    if n_clicks is None:
         raise dash.exceptions.PreventUpdate
+    
+    new_usuario = mongo.create_usuario(usuario=usuario,contrasena=contrasena)
+    return f'{new_usuario}'
+
+    
+
+@callback(
+    [Output('url', 'pathname'),
+     Output('error-message','children')],
+    [State('input-usuario','value'),
+     State('input-contrasena','value')],
+    Input('btn-log-in', 'n_clicks'),
+    prevent_initial_call=True
+)
+def iniciar_sesion(usuario, contrasena, n_clicks):
+    if n_clicks is None:
+        raise dash.exceptions.PreventUpdate
+    
+    usuario_valido = mongo.iniciar_sesion(usuario=usuario, contrasena=contrasena)
+    
+    if usuario_valido:
+        return '/cuestionario',''  # Modify this path if necessary
+    else:
+        return '/','Usuario o Contraseña incorrecta'  # Redirect to the default location if usuario is False
+
     
