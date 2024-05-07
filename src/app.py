65fca4123c6a7ee79dc48c7344dfcaee615516e1
drincon1 <d.rincon@uniandes.edu.co>
2024-05-06 19:21:30 -0500

    home page

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
 import dash
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Input, Output
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
-
+app = Dash(__name__,pages_folder="pages",use_pages=True,suppress_callback_exceptions=True)
 server = app.server
 
-app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+
+app.layout = html.Div(children=[
+	dash.page_container
 ])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
